99b1c098c65969f8feef5e83da1c52ca64198ca2
Steve Purcell <steve@sanityinc.com>
2023-07-18 08:29:42 +0200

    Add more grid layout variations

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ tiling.set('layouts', {
 -- TODO: maybe prefer http://www.hammerspoon.org/docs/hs.grid.html
 local positions = {
   maximized = hs.layout.maximized,
-  centered = {x=0, y=0, w=1, h=1},
+  centered = {x=0.34, y=0, w=0.33, h=1},
 
   left34 = {x=0, y=0, w=0.34, h=1},
   left50 = hs.layout.left50,
@@ -28,44 +28,54 @@ local positions = {
   right66 = {x=0.34, y=0, w=0.66, h=1},
 
   upper50 = {x=0, y=0, w=1, h=0.5},
+  upper50Left34 = {x=0, y=0, w=0.34, h=0.5},
+  upper50Left66 = {x=0, y=0, w=0.66, h=0.5},
   upper50Left50 = {x=0, y=0, w=0.5, h=0.5},
   upper50Right50 = {x=0.5, y=0, w=0.5, h=0.5},
+  upper50Right34 = {x=0.66, y=0, w=0.34, h=0.5},
+  upper50Right66 = {x=0.34, y=0, w=0.66, h=0.5},
 
   lower50 = {x=0, y=0.5, w=1, h=0.5},
   lower50Left50 = {x=0, y=0.5, w=0.5, h=0.5},
-  lower50Right50 = {x=0.5, y=0.5, w=0.5, h=0.5}
+  lower50Left34 = {x=0, y=0.5, w=0.34, h=0.5},
+  lower50Left66 = {x=0, y=0.5, w=0.66, h=0.5},
+  lower50Right50 = {x=0.5, y=0.5, w=0.5, h=0.5},
+  lower50Right34 = {x=0.66, y=0.5, w=0.34, h=0.5},
+  lower50Right66 = {x=0.34, y=0.5, w=0.66, h=0.5}
 }
 
 local grid = {
-  {key="q", units={positions.upper50Left50}},
+  {key="q", units={positions.upper50Left50, positions.upper50Left66, positions.upper50Left34}},
   {key="w", units={positions.upper50}},
-  {key="e", units={positions.upper50Right50}},
+  {key="e", units={positions.upper50Right50, positions.upper50Right66, positions.upper50Right34}},
 
   {key="a", units={positions.left50, positions.left66, positions.left34}},
-  {key="s", units={positions.centered, positions.maximized}},
+  {key="s", units={positions.maximized, positions.centered}},
   {key="d", units={positions.right50, positions.right66, positions.right34}},
 
-  {key="z", units={positions.lower50Left50}},
+  {key="z", units={positions.lower50Left50, positions.lower50Left66, positions.lower50Left34}},
   {key="x", units={positions.lower50}},
-  {key="c", units={positions.lower50Right50}}
+  {key="c", units={positions.lower50Right50, positions.lower50Right66, positions.lower50Right34}}
 }
+
+local rectToGeom = function(unit)
+  local screen = hs.screen.mainScreen()
+  return hs.geometry.new(unit):fromUnitRect(screen:frame()):floor()
+end
 
 hs.fnutils.each(grid, function(entry)
   hotkey.bind(mash, entry.key, function()
     local units = entry.units
-    local screen = hs.screen.mainScreen()
     local window = hs.window.focusedWindow()
-    local windowGeo = window:frame()
+    local windowGeo = window:frame():floor()
 
     local index = 0
     hs.fnutils.find(units, function(unit)
       index = index + 1
-
-      local geo = hs.geometry.new(unit):fromUnitRect(screen:frame()):floor()
+      local geo = rectToGeom(unit)
       return windowGeo:equals(geo)
     end)
     if index == #units then index = 0 end
-
     window:moveToUnit(units[index + 1])
   end)
 end)
