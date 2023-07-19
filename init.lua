34a3f77e74c76b138b0b55044bd356960671ee07
Steve Purcell <steve@sanityinc.com>
2023-07-19 11:50:07 +0200

    Support other 9x9 grid alignments

--- init.lua
+++ init.lua
@@ -17,7 +17,8 @@ tiling.set('layouts', {
 -- TODO: maybe prefer http://www.hammerspoon.org/docs/hs.grid.html
 local positions = {
   maximized = hs.layout.maximized,
-  centered = {x=0.33, y=0, w=0.34, h=1},
+  centerCol = {x=0.33, y=0, w=0.34, h=1},
+  centerRow = {x=0, y=0.33, w=1, h=0.34},
 
   left33 = {x=0, y=0, w=0.33, h=1},
   left50 = hs.layout.left50,
@@ -28,33 +29,42 @@ local positions = {
   right67 = {x=0.33, y=0, w=0.67, h=1},
 
   upper50 = {x=0, y=0, w=1, h=0.5},
+  upper33 = {x=0, y=0, w=1, h=0.33},
+  upper67 = {x=0, y=0, w=1, h=0.67},
+
   upper50Left33 = {x=0, y=0, w=0.33, h=0.5},
   upper50Left67 = {x=0, y=0, w=0.67, h=0.5},
   upper50Left50 = {x=0, y=0, w=0.5, h=0.5},
+
   upper50Right50 = {x=0.5, y=0, w=0.5, h=0.5},
   upper50Right33 = {x=0.67, y=0, w=0.33, h=0.5},
   upper50Right67 = {x=0.33, y=0, w=0.67, h=0.5},
 
   lower50 = {x=0, y=0.5, w=1, h=0.5},
+  lower67 = {x=0, y=0.33, w=1, h=0.67},
+  lower33 = {x=0, y=0.67, w=1, h=0.33},
+
   lower50Left50 = {x=0, y=0.5, w=0.5, h=0.5},
   lower50Left33 = {x=0, y=0.5, w=0.33, h=0.5},
   lower50Left67 = {x=0, y=0.5, w=0.67, h=0.5},
+
   lower50Right50 = {x=0.5, y=0.5, w=0.5, h=0.5},
   lower50Right33 = {x=0.67, y=0.5, w=0.33, h=0.5},
   lower50Right67 = {x=0.33, y=0.5, w=0.67, h=0.5}
 }
 
+-- Repeated presses of these keys cycle the selected window through the listed placements,
 local grid = {
   {key="q", units={positions.upper50Left50, positions.upper50Left67, positions.upper50Left33}},
-  {key="w", units={positions.upper50}},
+  {key="w", units={positions.upper50, positions.upper67, positions.upper33}},
   {key="e", units={positions.upper50Right50, positions.upper50Right67, positions.upper50Right33}},
 
   {key="a", units={positions.left50, positions.left67, positions.left33}},
-  {key="s", units={positions.maximized, positions.centered}},
+  {key="s", units={positions.maximized, positions.centerCol, positions.centerRow}},
   {key="d", units={positions.right50, positions.right67, positions.right33}},
 
   {key="z", units={positions.lower50Left50, positions.lower50Left67, positions.lower50Left33}},
-  {key="x", units={positions.lower50}},
+  {key="x", units={positions.lower50, positions.lower67, positions.lower33}},
   {key="c", units={positions.lower50Right50, positions.lower50Right67, positions.lower50Right33}}
 }
 
