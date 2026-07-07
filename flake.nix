1873763257bedaab99e4d4a63a48cf8c40901406
Steve Purcell <steve@sanityinc.com>
2026-07-07 11:36:23 +0100

    Flake updates

--- flake.nix
+++ flake.nix
@@ -5,20 +5,27 @@
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }@inputs: {
-    devShells."aarch64-darwin".default =
-      let
-        pkgs = import nixpkgs { system = "aarch64-darwin"; };
-      in
-        pkgs.mkShell {
-            buildInputs = with pkgs; [ lua luaPackages.luacheck lua-language-server ];
-            shellHook = ''
-              LUA_PATH=$LUA_PATH:/Applications/Hammerspoon.app/Contents/Resources/extensions
-              LUA_CPATH=$LUA_CPATH:/Applications/Hammerspoon.app/Contents/Resources/extensions
-              export LUA_PATH
-              export LUA_CPATH
-            '';
-
-        };
-  };
+  outputs = { self, nixpkgs }@inputs:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-darwin" "x86_64-darwin" ];
+    in
+    {
+      devShells = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          {
+            default = pkgs.mkShell
+              {
+                buildInputs = with pkgs; [ lua luaformatter luaPackages.luacheck lua-language-server ];
+                shellHook = ''
+                  LUA_PATH=$LUA_PATH:/Applications/Hammerspoon.app/Contents/Resources/extensions
+                  LUA_CPATH=$LUA_CPATH:/Applications/Hammerspoon.app/Contents/Resources/extensions
+                  export LUA_PATH
+                  export LUA_CPATH
+                '';
+              };
+          });
+    };
 }
